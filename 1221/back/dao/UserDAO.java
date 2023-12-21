1b298607ade7764a54f65c8663d29251eea249ef
minji12344 <140360630+minji12344@users.noreply.github.com>
2023-12-21 18:19:33 +0900

    Add files via upload

---
+++ 1221/back/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package com.example.dao;
+
+import java.util.*;
+
+import com.example.domain.QueryVO;
+import com.example.domain.UserVO;
+
+public interface UserDAO {
+	public List<UserVO> list(QueryVO vo);
+	public UserVO read(String uid);
+	public void insert(UserVO vo);
+	public void update(UserVO vo);
+	public int total(QueryVO vo);
+}
